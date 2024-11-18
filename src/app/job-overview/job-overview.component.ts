0c1ff35f1393683d75ef2daf91fb6bf362f2da86
Koen <koen.vochten@vdab.be>
2024-11-18 09:49:45 +0100

    list al jobs. generate job overview component

---
+++ src/app/job-overview/job-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './job-overview.component.html',
+  styleUrl: './job-overview.component.css'
+})
+export class JobOverviewComponent {
+
+}
